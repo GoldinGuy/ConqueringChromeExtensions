39217fa983506e3352014c4a0e73ed7d54163696
Seth <sethgoldin27@gmail.com>
2021-09-25 10:53:11 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Refreshapalooza
+
+A chrome extension lab for the Hackapalooza "Conquering Chrome Extensions" workshop.
+
+Here are some more awesome resources to review:
+
+- ["Conquering Chrome Extensions" Powerpoint](https://docs.google.com/presentation/d/1NRkjRCOMCr7UJd68WIn55IbtgIiQw_SVu5pvIUoxp1U/edit?usp=sharing)
+- [Chrome Extension Docs](https://developer.chrome.com/docs/extensions)
+- [Chrome Web Developer Portal](https://chrome.google.com/webstore/devconsole/)
+- [Learning Manifest V3](https://github.com/kentbrew/learning-manifest-v3)
+- [Example V3 Chrome Extensions](https://github.com/GoogleChrome/chrome-extensions-samples/tree/main/examples)
+- [Example V2 Chrome Extensions](https://github.com/orbitbot/chrome-extensions-examples)
+
+This lab was created because @benthaw and I knew a company willing to pay $70 for a simple chrome extension that could refresh a webpage at a set interval. After attending this workshop, you can make that!
+
+This lab sample extension is based on @SocialWorker's awesome [AutoReloadPage](https://chrome.google.com/webstore/detail/auto-reload-page/cggooibcfafefodnecpcbgiebbjjpnpb/related)
--- background.js
+++ background.js
@@ -3,61 +3,146 @@ chrome.action.setBadgeBackgroundColor({ color: "#4125dd" });
 
 let reloads_holder = {};
 
-const YEP = 'Yep';
+const YEP = "Yep";
 const NOPE = "Nope";
 
 // Main auto-reload function
-chrome.storage.onChanged.addListener((whatChanged, area) => {
-    if(area === "local") {
-        const currentKey = Object.keys(whatChanged)[0];
-        const currentVal = whatChanged[currentKey].newValue;
+// chrome.storage.onChanged.addListener((whatChanged, area) => {
+//     if (area === "local") {
+//         const currentKey = Object.keys(whatChanged)[0];
+//         const currentVal = whatChanged[currentKey].newValue;
 
-        if (typeof currentVal.active === 'boolean') {
-            if (currentVal.active) {
-                // Start reloading current tab
-                const reloadInterval = currentVal.unit === 'min' ? currentVal.input * 60 * 1000 : currentVal.input * 1000;
+//         if (typeof currentVal.active === 'boolean') {
+//             if (currentVal.active) {
+//                 // Start reloading current tab
+//                 const reloadInterval = currentVal.unit === 'min' ? currentVal.input * 60 * 1000 : currentVal.input * 1000;
 
-                reloads_holder[currentKey] = chrome.alarms.create({
+//                 reloads_holder[currentKey] = chrome.alarms.create({
 
-									delayInMinutes: reloadInterval / 60000,
-									periodInMinutes: reloadInterval / 60000,
-                });
-                
-                chrome.alarms.onAlarm.addListener((alarm) => {
-                    console.log('triggered')
-					chrome.tabs.reload(parseInt(currentKey));
-			    });
+//                     delayInMinutes: reloadInterval / 60000,
+//                     periodInMinutes: reloadInterval / 60000,
+//                 });
 
-                
-                // reloads_holder[currentKey] = setInterval(() => {
-                //     // Refresh page
-                //     chrome.tabs.reload(parseInt(currentKey));
-                // }, reloadInterval);
-            } else {
-                // Stop reloading
-                reloads_holder[currentKey].clear()
-                // clearInterval(reloads_holder[currentKey]);
-            }
-        }
-    }
-});
+//                 chrome.alarms.onAlarm.addListener((alarm) => {
+//                     console.log('triggered')
+//                     chrome.tabs.reload(parseInt(currentKey));
+//                 });
+
+//                 // reloads_holder[currentKey] = setInterval(() => {
+//                 //     // Refresh page
+//                 //     chrome.tabs.reload(parseInt(currentKey));
+//                 // }, reloadInterval);
+//             } else {
+//                 // Stop reloading
+//                 reloads_holder[currentKey].clear()
+//                 // clearInterval(reloads_holder[currentKey]);
+//             }
+//         }
+//     }
+// });
 
 // Update badge text
 chrome.tabs.onActivated.addListener(() => {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        let tab = tabs[0];
+	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+		let tab = tabs[0];
 
-        chrome.storage.local.get(tab.id.toString(), (items) => {
-            if (items[tab.id] && items[tab.id].active) {
-                chrome.action.setBadgeText({ text: YEP });
-            } else {
-                chrome.action.setBadgeText({ text: NOPE });
-            }
-        });
-      });
+		chrome.storage.local.get(tab.id.toString(), (items) => {
+			if (items[tab.id] && items[tab.id].active) {
+				chrome.action.setBadgeText({ text: YEP });
+			} else {
+				chrome.action.setBadgeText({ text: NOPE });
+			}
+		});
+	});
 });
 
 // Catch tab removed and remove the related reloader.
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-    clearInterval(reloads_holder[tabId]);
+	clearInterval(reloads_holder[tabId]);
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	if (changeInfo.status == "complete") {
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+			chrome.storage.local.get(tab.id.toString(), (items) => {
+				if (items[tab.id] && items[tab.id].active) {
+					chrome.action.setBadgeText({ text: YEP });
+					reloads_holder[currentKey] = chrome.alarms.create({
+						delayInMinutes: reloadInterval / 60000,
+						periodInMinutes: reloadInterval / 60000,
+					});
+
+					chrome.alarms.onAlarm.addListener((alarm) => {
+						console.log("triggered");
+						chrome.tabs.reload(parseInt(currentKey));
+					});
+				} else {
+					chrome.action.setBadgeText({ text: NOPE });
+				}
+			});
+		});
+	}
+});
+
+// chrome.action.setBadgeText({ text: "NOPE" });
+// chrome.action.setBadgeBackgroundColor({ color: "#4125dd" });
+
+// let reloads_holder = {};
+
+// const YEP = 'Yep';
+// const NOPE = "Nope";
+
+// // Main auto-reload function
+// chrome.storage.onChanged.addListener((whatChanged, area) => {
+//     if(area === "local") {
+//         const currentKey = Object.keys(whatChanged)[0];
+//         const currentVal = whatChanged[currentKey].newValue;
+
+//         if (typeof currentVal.active === 'boolean') {
+//             if (currentVal.active) {
+//                 // Start reloading current tab
+//                 const reloadInterval = currentVal.unit === 'min' ? currentVal.input * 60 * 1000 : currentVal.input * 1000;
+
+//                 reloads_holder[currentKey] = chrome.alarms.create({
+
+// 									delayInMinutes: reloadInterval / 60000,
+// 									periodInMinutes: reloadInterval / 60000,
+//                 });
+
+//                 chrome.alarms.onAlarm.addListener((alarm) => {
+//                     console.log('triggered')
+// 					chrome.tabs.reload(parseInt(currentKey));
+// 			    });
+
+//                 // reloads_holder[currentKey] = setInterval(() => {
+//                 //     // Refresh page
+//                 //     chrome.tabs.reload(parseInt(currentKey));
+//                 // }, reloadInterval);
+//             } else {
+//                 // Stop reloading
+//                 reloads_holder[currentKey].clear()
+//                 // clearInterval(reloads_holder[currentKey]);
+//             }
+//         }
+//     }
+// });
+
+// // Update badge text
+// chrome.tabs.onActivated.addListener(() => {
+//     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//         let tab = tabs[0];
+
+//         chrome.storage.local.get(tab.id.toString(), (items) => {
+//             if (items[tab.id] && items[tab.id].active) {
+//                 chrome.action.setBadgeText({ text: YEP });
+//             } else {
+//                 chrome.action.setBadgeText({ text: NOPE });
+//             }
+//         });
+//       });
+// });
+
+// // Catch tab removed and remove the related reloader.
+// chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+//     clearInterval(reloads_holder[tabId]);
+// });
